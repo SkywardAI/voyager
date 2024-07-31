aca1665dc24385cac07834551becc512c498a2bb
cbh778899 <cbh778899@outlook.com>
2024-07-31 17:47:28 +1000

    add post /completions
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- routes/inference.js
+++ routes/inference.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import { chatCompletion } from "../actions/inference.js";
 
 export default function inferenceRoute() {
     const router = Router();
+
+    router.post('/completions', chatCompletion);
+
     return router;
 }

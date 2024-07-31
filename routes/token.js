87bc9947afcb251b10b2b76a4992990b2d135843
Bohan Cheng <47214785+cbh778899@users.noreply.github.com>
2024-07-31 18:07:23 +1000

    add api to get api key (#4)
    * add route for generate api key
    
    Signed-off-by: cbh778899 <cbh778899@outlook.com>
    
    * update doc
    
    Signed-off-by: cbh778899 <cbh778899@outlook.com>
    
    ---------
    
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- routes/token.js
+++ routes/token.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
+import { generateAPIKey } from "../tools/generator.js";
 
 export default function tokenRoute() {
     const router = Router();
+
+    router.get('/api-key', (_, res)=>{
+        res.send({api_key: generateAPIKey()})
+    })
+
     return router;
 }

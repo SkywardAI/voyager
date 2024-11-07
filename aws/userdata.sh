4c0dcd1e275e3a732bf7fd83f36922462126894c
Duy Nguyen <41311465+RenuzitV@users.noreply.github.com>
2024-11-07 10:56:22 +0700

    feat: remove make dependancy feat: add chatbox in swagger

--- aws/userdata.sh
+++ aws/userdata.sh
@@ -1,16 +1,17 @@
-sudo yum install git docker -y
-sudo yum groupinstall "Development Tools" -y
+yum install git docker -y
 
 export DOCKER_CONFIG="$HOME/.docker"
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.30.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 
-sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
-sudo service docker start
+service docker start
 
 git clone https://github.com/RenuzitV/voyager.git
 
 cd voyager
 
-sudo make up
+chmod 777 ./setup/setup.sh
+
+./setup/setup.sh
